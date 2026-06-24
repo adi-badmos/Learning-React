9530f7434470dd951e0b9aa455b53e8ad580e590
adi-badmos <adityapratap141003@gmail.com>
2026-06-24 14:00:36 +0530

    Designed the Not Found Page

--- React/ecommerce-project/src/pages/NotFoundPage.jsx
+++ React/ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,11 +1,18 @@
 import { Header } from '../components/Header.jsx';
+import HomeFavicon from '../assets/images/home-favicon.png';
+import './NotFoundPage.css';
 
 export function NotFoundPage() {
     return (
         <>
             <title>404 Error: Page Not Found</title>
+            <link rel="icon" type="image/svg+xml" href={HomeFavicon} />
 
             <Header />
+
+            <div className="not-found-message">
+                Page not found
+            </div>
         </>
     );
 }
