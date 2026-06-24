6134a68b12cc57a4a836bdda7ec1cc57a830ee6b
adi-badmos <adityapratap141003@gmail.com>
2026-06-24 13:56:30 +0530

    Added Not Found Page

---
+++ React/ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../components/Header.jsx';
+
+export function NotFoundPage() {
+    return (
+        <>
+            <title>404 Error: Page Not Found</title>
+
+            <Header />
+        </>
+    );
+}
