0ab9e8ad661aa1ec6dac0452456c4b6e0175f280
adi-badmos <adityapratap141003@gmail.com>
2026-06-25 12:07:35 +0530

    Use axios to fetch data

--- React/ecommerce-project/src/pages/HomePage.jsx
+++ React/ecommerce-project/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../data/products.js';
 import HomeFavicon from '../assets/images/home-favicon.png';
@@ -5,6 +6,11 @@ import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
 export function HomePage() {
+    axios.get('http://localhost:3000/api/products')
+        .then((response) => {
+            console.log(response.data);
+        })
+
     return (
         <>
             <title>Amazun</title>
