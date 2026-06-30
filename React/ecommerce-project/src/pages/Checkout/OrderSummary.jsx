b9689b5b41e14263aaf6657831c54f45ba844235
adi-badmos <adityapratap141003@gmail.com>
2026-06-30 16:26:59 +0530

    Create delete item feature

--- React/ecommerce-project/src/pages/Checkout/OrderSummary.jsx
+++ React/ecommerce-project/src/pages/Checkout/OrderSummary.jsx
@@ -6,13 +6,12 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
     return (
         <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
-
                 return (
                     <div key={cartItem.productId} className="cart-item-container">
                         <DeliveryDate deliveryOptions={deliveryOptions} cartItem={cartItem} />
 
                         <div className="cart-item-details-grid">
-                            <CartItemDetails cartItem={cartItem} />
+                            <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
                             <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
                         </div>
                     </div>
